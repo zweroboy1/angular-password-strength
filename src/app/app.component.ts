7c892ad57ef287f4025a1b7b3adc56ea7ebe5c8d
zweroboy1 <fprognoz@gmail.com>
2024-02-18 18:28:39 +0200

    feat: add password indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PasswordInputComponent } from './password-input/password-input.component';
+import { PasswordIndicatorComponent } from './password-indicator/password-indicator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PasswordInputComponent],
+  imports: [RouterOutlet, PasswordInputComponent, PasswordIndicatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
