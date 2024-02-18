296870c7b9110b2d10115475516d0cecf2fdaec0
zweroboy1 <fprognoz@gmail.com>
2024-02-18 22:18:59 +0200

    test: passwordService.getPasswordStrength

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-password-strength');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular password strength checker');
   });
 });
--- src/app/services/password.service.spec.ts
+++ src/app/services/password.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
-import { PasswordService } from './password.service';
+import { PasswordService, PasswordStrength } from './password.service';
 
 describe('PasswordService', () => {
-  let service: PasswordService;
+  let passwordService: PasswordService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(PasswordService);
+    passwordService = TestBed.inject(PasswordService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  const passwordTestCases: { password: string, strength: PasswordStrength }[] = [
+    { password: '', strength: PasswordStrength.Empty },
+    { password: 'abc', strength: PasswordStrength.Short },
+    { password: '5432', strength: PasswordStrength.Short },
+    { password: '123aaй!', strength: PasswordStrength.Short },
+    { password: 'password', strength: PasswordStrength.Easy },
+    { password: 'чемпионат', strength: PasswordStrength.Easy },
+    { password: '12345678', strength: PasswordStrength.Easy },
+    { password: '!@#$%^&*', strength: PasswordStrength.Easy },
+    { password: 'pass@word', strength: PasswordStrength.Medium },
+    { password: 'буквы1122', strength: PasswordStrength.Medium },
+    { password: '$1111$$$$$', strength: PasswordStrength.Medium },
+    { password: 'P@ssw0rd', strength: PasswordStrength.Strong },
+    { password: 'ФФ№№№ааа11ssw0rd', strength: PasswordStrength.Strong },
+  ];
+
+  passwordTestCases.forEach(testCase => {
+    it(`should return ${testCase.strength} for password '${testCase.password}'`, () => {
+      const result = passwordService.getPasswordStrength(testCase.password);
+      expect(result).toBe(testCase.strength);
+    });
   });
 });
